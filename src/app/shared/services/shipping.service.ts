1763e04ac1c998914cd23cd0b2ff394b9f4a2081
adsoftsito <adsoft@live.com.mx>
2019-03-11 12:56:33 -0600

    update shipping save

--- src/app/shared/services/shipping.service.ts
+++ src/app/shared/services/shipping.service.ts
@@ -1,8 +1,11 @@
-import {
+/*import {
   AngularFireList,
   AngularFireObject,
   AngularFireDatabase
 } from "angularfire2/database";
+*/
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+
 import { Billing } from "./../models/billing";
 import { Injectable } from "@angular/core";
 
@@ -10,31 +13,39 @@ import { Injectable } from "@angular/core";
   providedIn: "root"
 })
 export class ShippingService {
-  shippings: AngularFireList<Billing>;
-  shipping: AngularFireObject<Billing>;
-  constructor(private db: AngularFireDatabase) {
+
+  shippings: AngularFirestoreCollection<Billing>;
+  shipping: AngularFirestoreDocument<Billing>;
+
+ // products: AngularFirestoreCollection<Product>;
+	//product:  AngularFirestoreDocument<Product>;
+
+
+  constructor(private db: AngularFirestore) {
     this.getshippings();
   }
 
   createshippings(data: Billing) {
-    this.shippings.push(data);
+    //this.shippings.push(data);
+    this.db.collection('shippings').add(data);
+
   }
 
   getshippings() {
-    this.shippings = this.db.list("shippings");
-    return this.shippings;
+   // this.shippings = this.db.list("shippings");
+   // return this.shippings;
   }
 
   getshippingById(key: string) {
-    this.shipping = this.db.object("products/" + key);
-    return this.shipping;
+   // this.shipping = this.db.object("products/" + key);
+   // return this.shipping;
   }
 
   updateshipping(data: Billing) {
-    this.shippings.update(data.$key, data);
+    //this.shippings.update(data.$key, data);
   }
 
   deleteshipping(key: string) {
-    this.shippings.remove(key);
+    //this.shippings.remove(key);
   }
 }
